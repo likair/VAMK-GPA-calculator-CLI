98b7ffd48e55e05ad86bd2a552a170475f3f8a57
Lebs <lebs.fi@qq.com>
2015-05-24 17:48:37 +0300

    Calc percent and other small changes

--- calcGPAByWeb.py
+++ calcGPAByWeb.py
@@ -4,7 +4,7 @@ Created on May 21, 2015
 
 @author: Likai
 '''
-import re,urllib, http.cookiejar,getpass,platform
+import re, urllib, http.cookiejar, getpass, platform, functools, sys
 
 def GetHtmlFormVAMK(stuID, password):
     loginCookie = http.cookiejar.LWPCookieJar()
@@ -15,10 +15,12 @@ def GetHtmlFormVAMK(stuID, password):
     leftFrameUrl = "https://secure.puv.fi/wille/emainval.asp"
     requestUrl = "https://secure.puv.fi/wille/eWilleNetLink.asp?Link=https://secure.puv.fi/willenet/HopsSuoritukset.aspx&Hyv=1&Opjakso=&ArvPvm="
     requestData = urllib.parse.urlencode({'rbRajaus' : 'rbSuoritukset' , 'dfOpjakso' : '' , 'dfArviointipvm' : ''}).encode()
-
     opener.open(startUrl)
+    print('.....', end='', flush=True)
     opener.open(loginUrl, loginData)
+    print('......', end='', flush=True)
     opener.open(leftFrameUrl)
+    print('......', file=sys.stdout, flush=True)
     return(opener.open(requestUrl, requestData).read().decode('utf-8'))
 
 
@@ -26,18 +28,17 @@ def extractCourses(text):
     coursesList = []
     courses = re.findall(r'["d]>\d+,\d+ [CO].*?>[\dMS]<', text)
     for course in courses:
-        course = re.findall(r'\d+\.\d+|&nbsp;|[\dMS]', course.replace(',','.'))
+        course = re.findall(r'\d+\.\d+|&nbsp;|[\dMS]', course.replace(',', '.'))
         course.pop(1)
         course.pop(1)
         coursesList.append(course)
     return coursesList
 
-def gpaCalcByCoursesList(coursesList):
+def generateReport(coursesList):
     gpa = 0
     normalCredits = 0
     MCredits = 0
     sCredits = 0
-
     CoursesGrades = [0, 0, 0, 0, 0, 0, 0, 0]
     if coursesList != []:
         for course in coursesList:
@@ -54,30 +55,48 @@ def gpaCalcByCoursesList(coursesList):
                 normalCredits += eval(course[0])
                 CoursesGrades[eval(course[1])] += 1
         gpa /= normalCredits
-    print('*Your GPA: {:4.3f}'.format(gpa))
-    print('-----------------Details------------------')
-    print('Total credits: ' + str(normalCredits + MCredits + sCredits))
-    print('Normal credits: ' + str(normalCredits))
-    print('M credits: ' + str(MCredits))
-    print('S credits: ' + str(sCredits))
-    print('Grades summary:')
-    for i in range(6):
-        print('  Grade {}: {}'.format(i, str(CoursesGrades[i])))
-    print('  Grade M: ' + str(CoursesGrades[6]))
-    print('  Grade S: ' + str(CoursesGrades[7]))
+        totalCourses = functools.reduce(lambda x, y: x + y, CoursesGrades)
+        print('------------------------------------------')
+        print('             *REPORT CARD*\n')
+        if 0 <= gpa < 0.5:
+            print('- Oh, what happened?')
+        if 0.5 <= gpa < 1.5 :
+            print('- Give me a reason, how it is possible?')
+        elif 1.5 <= gpa < 2.5:
+            print('- Hey, poor guy, you shouldn\'t be like this!')
+        elif 2.5 <= gpa < 3.5:
+            print('- Come on, You are very promising, but should study harder. ')
+        elif 3.5 <= gpa < 4.5:
+            print('- OK, you own an enviable GPA!')
+        elif 4.5 <= gpa < 5:
+            print('- Gorgeous! I must admit that you are a genius!')
+        print('* Your GPA: {:4.3f}'.format(gpa))
+        print('-----------------DETAILS------------------')
+        print('* Total credits: {:5}'.format(normalCredits + MCredits + sCredits))
+        print('* Normal credits:{:5}'.format(normalCredits))
+        print('* M credits:     {:5}'.format(MCredits))
+        print('* S credits:     {:5}'.format(sCredits))
+        print('* Grades summary:')
+        for i in range(6):
+            print('  - Grade {}: {:4} ({:6.2%})'.format(i, CoursesGrades[i], CoursesGrades[i] / totalCourses))
+        print('  - Grade {}: {:4} ({:6.2%})'.format('M', CoursesGrades[6], CoursesGrades[6] / totalCourses))
+        print('  - Grade {}: {:4} ({:6.2%})'.format('S', CoursesGrades[7], CoursesGrades[7] / totalCourses))
+    else:
+        print('No course!')
 
 if __name__ == '__main__':
     while True:
         try:
             print('**************GPA in VAMK*****************')
-            stuID = input('Input your student ID: ')
-            password = getpass.getpass('Input your password: ')
+            print('* AUTHENTICATION')
+            stuID = input('  Student ID: ').strip()
+            password = getpass.getpass('  Password: ').strip()
             if stuID == '' or password == '':
                 raise Exception
-            print('Please Wait for a moment..................\n')
-            gpaCalcByCoursesList(extractCourses(GetHtmlFormVAMK(stuID, password)))
+            print('Please Wait for a moment.', end='', file=sys.stdout, flush=True)
+            generateReport(extractCourses(GetHtmlFormVAMK(stuID, password)))
         except:
-            print('Error!')
+            print('\nError! Please confirm your student ID/password and if there is network connection.')
         finally:
             print('------------------------------------------')
         if(platform.system() == 'Linux' or input('Input "q" to continue, or any other key to continue:') == 'q'):
